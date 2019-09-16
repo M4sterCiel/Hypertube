bcad3c90fd58b7e9bfcd3bc4a4c4f2c64163d442
Lucas Cordenod <Lucas@MacBook-de-Lucas.local>
2019-09-16 20:35:45 +0200

    Tweaked front for login/register pages

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
-import "materialize-css";
-import "materialize-css/dist/css/materialize.min.css";
-import "./LandingPage.scss";
-import Navbar from "../navbar/NavBar";
-import { LpBigButton } from "../buttons/BigButtons";
-import { withRouter } from "react-router-dom";
+import React, { Component } from 'react';
+import './LandingPage.scss';
+import Navbar from '../navbar/NavBar';
+import { LpBigButton } from '../buttons/BigButtons';
+import { withRouter } from 'react-router-dom';
 
 class LandingPage extends Component {
   render() {
