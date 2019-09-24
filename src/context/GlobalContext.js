aaa4f238a63449f8f44909a23aca554a590d25b9
raphael <raphael.allemand@live.fr>
2019-09-24 16:03:12 +0200

    creating context store

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,4 +1,6 @@
 import React, { createContext, Component } from "react";
+import axios from "axios";
+import AuthService from "../services/AuthService";
 
 export const GlobalContext = createContext({
   locale: "en",
@@ -13,26 +15,49 @@ export const GlobalContext = createContext({
 });
 
 class GlobalContextProvider extends Component {
-  state = {
-    locale: "en",
-    username: "",
-    firstname: "",
-    lastname: "",
-    email: "",
-    uid: "",
-    picture: "",
-    setLocale: data => this.setState({ locale: data }),
-    updateContext: data =>
-      this.setState({
-        locale: data.locale,
-        username: data.username,
-        firstname: data.firstname,
-        lastname: data.lastname,
-        email: "",
-        uid: data.username,
-        picture: data.picture
-      })
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      locale: "en",
+      username: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      uid: "",
+      picture: "",
+      setLocale: data => this.setState({ locale: data }),
+      updateContext: data =>
+        this.setState({
+          locale: data.locale,
+          username: data.username,
+          firstname: data.firstname,
+          lastname: data.lastname,
+          email: "",
+          uid: data.username,
+          picture: data.picture
+        })
+    };
+    this.Auth = new AuthService();
+  }
+
+  async componentDidMount() {
+    var token = await this.Auth.getToken();
+    if (token) {
+      axios
+        .get("/users/session", { headers: { Authorization: token } })
+        .then(res => {
+          this.setState({
+            locale: res.data.language ? res.data.language : "en",
+            username: res.data.username ? res.data.username : "",
+            fisrtname: res.data.fisrtname ? res.data.fisrtname : "",
+            lastname: res.data.lastname ? res.data.lastname : "",
+            email: res.data.email ? res.data.email : "",
+            uid: res.data._id ? res.data._id : "",
+            picture: res.data.picture ? res.data.picture : ""
+          });
+        });
+    }
+  }
 
   render() {
     return (
