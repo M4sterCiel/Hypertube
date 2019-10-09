082ae81a4aad0fefbb9421056bb0ab4f96393fe6
Martin <mrtscg@gmail.com>
2019-10-09 14:31:43 +0200

    [PUSH] delete button done

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -16,21 +16,20 @@ export default class AuthService {
 
   // Checking if token exists and is still valid
   loggedIn = async () => {
-    // const token = await this.getToken();
-    // if (!token) {
-    //   return false;
-    // }
-    // var valid = false;
-    // await axios
-    //   .get("/users/session", { headers: { Authorization: token } })
-    //   .then(res => {
-    //     if (res.data._id) valid = true;
-    //   })
-    //   .catch(err => {
-    //     return false;
-    //   });
-    // return !!token && !this.isTokenExpired(token) && valid;
-    return true;
+    const token = await this.getToken();
+    if (!token) {
+      return false;
+    }
+    var valid = false;
+    await axios
+      .get("/users/session", { headers: { Authorization: token } })
+      .then(res => {
+        if (res.data._id) valid = true;
+      })
+      .catch(err => {
+        return false;
+      });
+    return !!token && !this.isTokenExpired(token) && valid;
   };
 
   // Checking if token exists and is still valid
